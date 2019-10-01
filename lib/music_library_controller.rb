d9ff6e21623b46c76e4644ae12a83e8988257fe4
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-01 00:28:45 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,7 +38,7 @@ class MusicLibraryController
   end
   
   def list_songs 
-    Song.all.sort {|artist_one, artist_two| artist_one.name <=> artist_two.name}.each_with_index
+    Song.all.sort {|artist_one, artist_two| artist_one.name <=> artist_two.name}.each_with_index(1)
   end 
 
 
